7a56d7c3c2fb6be39828eae2cf2fc9fff4875a9a
Hernan Y.Ke <yuriqiao@gmail.com>
2015-04-14 22:28:34 +0800

    3rd fix

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Hernan Y.Ke on 4/14/15.
+ */
+var gulp=require('gulp'),
+    nodemon=require('nodemon');
+
+
+gulp.task('default',function(){
+    nodemon({
+        script:'app.js',
+        ext:'js',
+        env:{
+            PORT:8000
+        },
+        ignore:['./node_modules/**']
+    })
+        .on('restart',function(){
+            console.log('Wating for restarting');
+        });
+});
